60983b2c1744b97a21e8b793c5b365da1b72d872
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2024-12-28 16:12:20 -0300

    test: add test for owner address in FundMe contract

--- test/FundMe.t.sol
+++ test/FundMe.t.sol
@@ -15,4 +15,8 @@ contract FundMeTest is Test {
     function testMinimumDollarIsFive() public view {
         assertEq(fundMe.MINIMUM_USD(), 5e18);
     }
+
+    function testOwnerIsMsgSender() public view {
+        assertEq(fundMe.i_owner(), address(this));
+    }
 }
