70757cde2522bb1e95c3ddf673a15d8dd2d6c917
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2024-12-28 15:51:55 -0300

    test: add test for minimum dollar value in FundMe contract

--- test/FundMe.t.sol
+++ test/FundMe.t.sol
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.18;
 
-import {Test} from 'forge-std/Test.sol';
+import {Test} from "forge-std/Test.sol";
 
-import { FundMe } from 'foundry-fund-me/FundMe.sol';
+import {FundMe} from "foundry-fund-me/FundMe.sol";
 
 contract FundMeTest is Test {
     FundMe public fundMe;
 
     function setUp() public {
         fundMe = new FundMe();
+    }
+
+    function testMinimumDollarIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
     }
 }
