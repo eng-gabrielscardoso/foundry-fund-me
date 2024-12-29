49a918333dfb3b075116b1baaeb0ba1e66e3538c
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2024-12-29 10:31:19 -0300

    feat: integrate HelperConfig for dynamic price feed address in FundMe script

--- scripts/FundMe.s.sol
+++ scripts/FundMe.s.sol
@@ -3,16 +3,20 @@ pragma solidity ^0.8.19;
 
 import {Script} from 'forge-std/Script.sol';
 import {FundMe} from "foundry-fund-me/FundMe.sol";
+import {HelperConfig} from './HelperConfig.s.sol';
 
 contract FundMeScript is Script {
     FundMe public fundMe;
+    HelperConfig public helperConfig = new HelperConfig();
 
     function setUp() public {}
 
     function run() external returns (FundMe) {
+        (address ethUsdPriceFeed) = helperConfig.activeNetworkConfig();
+
         vm.startBroadcast();
 
-        fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        fundMe = new FundMe(ethUsdPriceFeed);
 
         vm.stopBroadcast();
 
