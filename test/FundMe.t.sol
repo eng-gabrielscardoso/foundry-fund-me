3d92652b76c67d81daaec4afbac6e6e988e2ab50
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2025-01-03 12:09:08 -0300

    feat: add test for cheaper withdrawal from multiple funders in FundMeTest

--- test/FundMe.t.sol
+++ test/FundMe.t.sol
@@ -97,7 +97,7 @@ contract FundMeTest is Test {
         assertEq(startingOwnerBalance + startingFundMeBalance, endingOwnerBalance);
     }
 
-    function testWithdrawFromMultipleFunders() public funded {
+    function testWithdrawFromMultipleFundersExpensive() public funded {
         uint160 numberOfFunders = 10;
         uint160 startingFunderIndex = 1;
 
@@ -111,6 +111,26 @@ contract FundMeTest is Test {
 
         vm.startPrank(fundMe.getOwner());
         fundMe.withdraw();
+        vm.stopPrank();
+
+        assert(address(fundMe).balance == 0);
+        assert(startingFundMeBalance + startingOwnerBalance == fundMe.getOwner().balance);
+    }
+
+    function testWithdrawFromMultipleFundersCheaper() public funded {
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 1;
+
+        for (uint160 i = startingFunderIndex; i < numberOfFunders; i++) {
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        vm.startPrank(fundMe.getOwner());
+        fundMe.cheaperWithdraw();
         vm.stopPrank();
 
         assert(address(fundMe).balance == 0);
